5b1eb50f67e9515d216fb5c3f1641d262122c996
LimaIsaias <coelholimaisaias@gmail.com>
2021-09-23 08:52:41 -0300

    renomear entidade Model > Terminal

--- src/main/java/br/com/limaisaias/dockdesafio/repository/ModelRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.limaisaias.dockdesafio.repository;
-
-import br.com.limaisaias.dockdesafio.model.Model;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<Model, Integer> {
-}
